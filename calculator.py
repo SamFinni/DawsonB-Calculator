deaae7cc85fb82919b02936787997eb2285f7af3
Samual <samual@finnigan.me>
2020-09-18 12:10:05 -0700

    Created calculator skeleton code

---
+++ calculator.py
@@ -0,0 +1,46 @@
+def Add(a, b):
+	#TODO
+	return "TODO"
+
+def Subtract(a, b):
+	#TODO
+	return "TODO"
+
+def Multiply(a, b):
+	#TODO
+	return "TODO"
+
+def Divide(a, b):
+	#TODO
+	return "TODO"
+
+###
+# start of execution
+###
+
+##
+# tests (3 per operation)
+##
+
+#TODO
+
+##
+# user input/output
+##
+
+eq = input("Enter a single-operator equation (eg, 5*2.5): ")
+
+result = "Result: "
+
+if ("+" in eq):
+	result += Add(float(eq.split("+")[0]), float(eq.split("+")[1]))
+elif ("-" in eq):
+	result += Subtract(float(eq.split("-")[0]), float(eq.split("-")[1]))
+elif ("*" in eq):
+	result += Multiply(float(eq.split("*")[0]), float(eq.split("*")[1]))
+elif ("/" in eq):
+	result += Divide(float(eq.split("/")[0]), float(eq.split("/")[1]))
+else:
+	result = "Invalid input"
+
+print(result)
