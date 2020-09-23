1e9d4f05135c5ccba98e3c2dc478ca57cee32851
Samual <samual@finnigan.me>
2020-09-22 18:43:48 -0700

    making sure all parts use the same coding standards, added a few comments

--- calculator.py
+++ calculator.py
@@ -1,19 +1,17 @@
-# Adds two numbers together (a + b)
 def Add(a, b):
+	# Adds two numbers together (a + b)
 	return str(round(a+b, 4))
 
 def Subtract(a, b):
-	#subtracts two numbers a - b
-	result = str(round(a-b,4))
-	return result
+	# subtracts two numbers (a - b)
+	return str(round(a-b, 4))
 
 def Multiply(a, b):
-	# evaluate function
-	result = round(a * b,4)
-	# return answer as string
-	return str(result)
+	# multiplies two numbers (a * b)
+	return str(round(a*b, 4))
 
 def Divide(a, b):
+	# divides two numbers (a / b)
 	try:
 		return str(round(a/b, 2))
 	except ZeroDivisionError:
@@ -24,12 +22,6 @@ def Divide(a, b):
 ###
 
 ##
-
-# Subtract(a,b) tests
-assert Subtract(10,5) == "5"
-assert Subtract(87.4,63.5) == "23.9"
-assert Subtract(-15,-34) == "19"
-
 # tests (3 per operation)
 ##
 
@@ -37,6 +29,11 @@ assert Subtract(-15,-34) == "19"
 assert Add(5.2, 57) == "62.2"
 assert Add(3626.236, 74.745) == "3700.981"
 assert Add(-3.5, -7.4) == "-10.9"
+
+# Subtract() tests
+assert Subtract(10, 5) == "5"
+assert Subtract(87.4, 63.5) == "23.9"
+assert Subtract(-15, -34) == "19"
 
 # Multiply() tests
 assert Multiply(4, 13) == "52"
@@ -60,6 +57,7 @@ while alive:
 
 	result = "Result: "
 
+	# find the operation to use
 	if ("+" in eq):
 		result += Add(float(eq.split("+")[0]), float(eq.split("+")[1]))
 	elif ("-" in eq):
@@ -71,8 +69,10 @@ while alive:
 	else:
 		result = "Invalid input"
 
+	# give user the result
 	print(result)
 
+	# prompt to quit or continue
 	quit = input("Quit [Y/N]: ")
 	print('')
 	if quit.lower() == 'y':
